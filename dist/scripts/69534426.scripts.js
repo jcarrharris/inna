1e1393bdeb635aa891ca79508f3e0b2441e70918
jcarrharris <joncarrharris@gmail.com>
2014-02-27 23:12:58 -0500

    added express server

---
+++ dist/scripts/69534426.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("webappApp",["ngCookies","ngResource","ngSanitize"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("webappApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
